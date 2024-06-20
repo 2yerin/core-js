008008a6cb3f32bf72a48a988bb9e4d377624920
2yerin <lyerin0113@naver.com>
2024-06-20 10:44:32 +0900

    스타벅스 메뉴(gsap version)

--- client/starbucks/main.js
+++ client/starbucks/main.js
@@ -1,32 +1,13 @@
+/*global gsap*/
+
 const aList = document.querySelectorAll('nav a');
-const depthList = document.querySelectorAll('.depth');
-const header = document.querySelector('#header');
-
-const h = t => t.style.height = 0;
-
-/*
-depthList.forEach((item)=>{
-  console.log(item);
-})
-
-depthList.forEach(console.log) //전달하려는 값이 같으면 이런식으로 줄여쓰기도 가능
-
-depthList.forEach(h) //이것도 가능
-*/
 
 aList.forEach((a)=>{
-  a.addEventListener('mouseenter',()=>{
-
     const target = a.lastElementChild;
 
-    
-    // 내가 선택한 depth를 제외한 항목 0
-    // 모든 depth 높이를 0 
-    depthList.forEach(h)
+    const t1 = gsap.timeline({paused:true})
+    .to(target,{height:100, ease:'power3.inOut'})
 
-    target.style.height = '100px';
-  })
-
+    a.addEventListener('mouseenter',()=>t1.play())
+    a.addEventListener('mouseleave',()=>t1.reverse())
 })
-
-header.addEventListener('mouseleave',()=>depthList.forEach(h))
